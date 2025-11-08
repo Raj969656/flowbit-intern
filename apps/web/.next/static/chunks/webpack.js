a6ff212f8e91bd760308170d0733e1ffa9052ea5
Raj Yadav <mr.rajyadav272207@gmail.com>
2025-11-09 04:01:46 +0530

    Fixed .gitignore and cleaned repo

--- apps/web/.next/static/chunks/webpack.js
+++ apps/web/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e231a2abbac08318"; }
+/******/ 		__webpack_require__.h = function() { return "0fd74647d32ddd95"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
